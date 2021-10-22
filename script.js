3ed4a7e921b8028e89be87c638a189b27f34546c
unknown <singhalsaksham.mzn@gmail.com>
2021-10-22 23:29:15 +0530

    Adding script for heroku

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = process.env.PORT || 80;
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/DanceContact', {useNewUrlParser: true, useUnifiedTopology: true});
